78d875f456d074cadd5fbfa3d9197cfc8dba2b9a
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-09-25 14:39:05 +0200

    innit

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ var sequence1Player = [];
 var sequence2Player = [];
 var sequence3Player = [];
 var sequence4Player = [];
-
+var round = 1;
 // View
 updateView();
 function updateView(){
     html = /*html*/ `
+    <div id="input" >DONE</div>
     <div id="box">
     <div id="box1" onclick="button(this)"></div>
     <div id="box2" onclick="button(this)"></div>
@@ -24,13 +25,14 @@ function updateView(){
 
     
     
-    <div id="center"></div>
+    <div id="center" onclick="getInput()"></div>
     <div id="center2"></div>
 
    
     
     
     </div><div id="center3"></div>
+    
     <input id="btn" type="button" value="Start" onclick="Start()"/>
     <input id="btn" type="button" value="reset" onclick="reset()"/>
     `;
@@ -153,6 +155,7 @@ function Start(){
     let place = numGen();
     sequence1.push(place);
     const myTime2 = setTimeout(responsive2, 1000, place);
+    //const inputInterval = setTimeout(getInput, 2000, sequence1);
     console.log(place);
     if(place == "0"){
         document.getElementById("box1").classList.add('lightUpYellow');
@@ -171,13 +174,15 @@ function Start(){
         document.getElementById("box4").classList.add('lightUpGreen');
         myTime2;
     }
-    getInput();
+    //inputInterval;
 }
 
 function round2(){
+    alert('runde 2');
+    round = 2;
     let place;
     let timeFactor = 1;
-    for(let i = 0; i<5; i++){
+    for(let i = 0; i<2; i++){
         place = numGen();
         sequence2[i]=place;
     }
@@ -226,6 +231,8 @@ function round2(){
     }
 }
 function round3(){
+    alert('runde 3');
+    round = 3;
     let place;
     let timeFactor = 1;
     for(let i = 0; i<3; i++){
@@ -276,81 +283,61 @@ function round3(){
     }
     }
 }
-
-function round32(){
+function round4(){
+    alert('runde 4');
+    round = 4;
     let place;
-    let timeFactor;
-    for(let i=0; i<3; i++){
+    let timeFactor = 1;
+    for(let i = 0; i<4; i++){
         place = numGen();
-        sequence3[i]=(place);
+        sequence4[i]=place;
     }
-    //let result3='';
-    for(let j=0; j <= sequence3.length; j++){   
+    let result4='';
+    for(let j=0; j <= sequence4.length; j++){   
 
-    if(sequence3[j] == "0"){
-        if(sequence3[0] == sequence3[1]){
+    if(sequence4[j] == "0"){
+        
+        if(sequence4[0] == sequence4[1]){
             timeFactor = 2;
-        }
-        else if(sequence3[1] == sequence3[2]){
-            timeFactor = 3;
         }
         setTimeout(function(){
          document.getElementById("box1").classList.add('lightUpYellow');
-        timeDelay2(j);   
+        timeDelay3(j);   
         }, 1000*j*timeFactor);
-        
-
     }
-    else if(sequence3[j] == "1"){
-        if(sequence3[0] == sequence3[1]){
+    else if(sequence4[j] == "1"){
+        if(sequence4[0] == sequence4[1]){
             timeFactor = 2;
-        }
-        else if(sequence3[1] == sequence3[2]){
-            timeFactor = 3;
         }
         setTimeout(function(){
           document.getElementById("box2").classList.add('lightUpBlue');
-        timeDelay2(j);  
+        timeDelay3(j);  
         }, 1000*j*timeFactor);
-        
     }
-    else if(sequence3[j] == "2"){
-        if(sequence3[0] == sequence3[1]){
+    else if(sequence4[j] == "2"){
+        if(sequence4[0] == sequence4[1]){
             timeFactor = 2;
-        }
-        else if(sequence3[1] == sequence3[2]){
-            timeFactor = 3;
         }
         setTimeout(function(){
           document.getElementById("box3").classList.add('lightUpRed');
-        timeDelay2(j);  
+        timeDelay3(j);  
         }, 1000*j*timeFactor);
         
     }
-    else if(sequence3[j] == "3"){
-        if(sequence3[0] == sequence3[1]){
+    else if(sequence4[j] == "3"){
+        if(sequence4[0] == sequence4[1]){
             timeFactor = 2;
-        }
-        else if(sequence3[1] == sequence3[2]){
-            timeFactor = 3;
         }
         setTimeout(function(){
           document.getElementById("box4").classList.add('lightUpGreen');
-        timeDelay2(j);  
+        timeDelay3(j);  
         }, 1000*j*timeFactor);
         
     }
     }
-
-    let result3='';
-    if(result3 == "win"){
-        round4();
-    }
-    else if(result3 == "lose"){
-        Start();
-    }
 }
-function round4(){
+
+/*function round4(){
     let result4='';
     if(result4 == "win"){
         round5();
@@ -367,18 +354,52 @@ function round5(){
     else if(result5 == "lose"){
         Start();
     }
-}
+}*/
 //user inputs
-function getInput(){
-    
-    document.getElementById("box1").addEventListener("click", function(){sequence1Player.push(0)})
-    document.getElementById("box2").addEventListener("click", function(){sequence1Player.push(1)})
-    document.getElementById("box3").addEventListener("click", function(){sequence1Player.push(2)})
-    document.getElementById("box4").addEventListener("click", function(){sequence1Player.push(3)})
-    
-    console.log(sequence1Player);
+document.getElementById("box1").addEventListener("click", function(){sequence1Player.push(0)});
+document.getElementById("box2").addEventListener("click", function(){sequence1Player.push(1)});
+document.getElementById("box3").addEventListener("click", function(){sequence1Player.push(2)});
+document.getElementById("box4").addEventListener("click", function(){sequence1Player.push(3)});
+
+function afterR1(){
+document.getElementById("box1").addEventListener("click", function(){sequence2Player.push(0)});
+document.getElementById("box2").addEventListener("click", function(){sequence2Player.push(1)});
+document.getElementById("box3").addEventListener("click", function(){sequence2Player.push(2)});
+document.getElementById("box4").addEventListener("click", function(){sequence2Player.push(3)});
+}
+function afterR2(){
+document.getElementById("box1").addEventListener("click", function(){sequence3Player.push(0)});
+document.getElementById("box2").addEventListener("click", function(){sequence3Player.push(1)});
+document.getElementById("box3").addEventListener("click", function(){sequence3Player.push(2)});
+document.getElementById("box4").addEventListener("click", function(){sequence3Player.push(3)});
+}
+function afterR3(){
+document.getElementById("box1").addEventListener("click", function(){sequence4Player.push(0)});
+document.getElementById("box2").addEventListener("click", function(){sequence4Player.push(1)});
+document.getElementById("box3").addEventListener("click", function(){sequence4Player.push(2)});
+document.getElementById("box4").addEventListener("click", function(){sequence4Player.push(3)});
 }
 
+function getInput(){
+    if(round == 1){evaluateStart();}
+    else if(round == 2){evaluateR1(sequence2, sequence2Player);}
+    else if(round == 3){evaluateR1(sequence3, sequence3Player);}
+    else if(round == 4){evaluateR1(sequence4, sequence4Player);}
+    else if(round == 5){evaluateR1();}
+}
+
+function evaluateStart(){
+    let result;
+    if(sequence1.length == sequence1Player.length && sequence1[0] == sequence1Player[0]){
+        console.log('win');
+        result = 'win';}
+        else{ console.log('lose'); result = 'lose';}
+    if( result == 'win'){
+        round2();
+        afterR1();}
+    else{alert('you lost'); reset();} 
+    return result;
+}
 
 //tids delays
 function timeDelay(j){
@@ -393,17 +414,28 @@ function timeDelay3(j){
 
 // evaluation
 
-function evaluate(pc, player){
+function evaluateR1(pc, player){
+    console.log('hei');
    let result = '';
-   for(let i=0; i<=player.length; i++){
+   for(let i=0; i<player.length; i++){
         if(pc[i] == player[i]){
             console.log("yes");
+            result = 'win';
+            
         }
-        else{ console.log("no");}
+        else{ console.log("no"); result = 'lose';}
         
    }
-    if( result == "win"){
-       round2(); 
+    if( round== 2 && result == "win"){
+       round3();
+       afterR2(); 
+    }
+    else if(round == 3 && result == "win"){
+        round4();
+        afterR3();
+    }
+    else if(round == 4 && result == "win"){
+        alert('du vant');
     }
     else if( result == "lose"){
         Start();
