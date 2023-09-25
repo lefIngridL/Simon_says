c579782e38f94c1d3744f425b05a22f8c7e91a76
lefIngridL <90441834+lefIngridL@users.noreply.github.com>
2023-09-25 09:47:08 +0200

    innit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Model
 const app = document.getElementById('app');
 var whatbox;
+var rightbox;
+var sequence1 = [];
+var sequence2 = [];
+var sequence3 = [];
+var sequence4 = [];
+var sequence1Player = [];
+var sequence2Player = [];
+var sequence3Player = [];
+var sequence4Player = [];
+
 // View
 updateView();
 function updateView(){
@@ -33,30 +43,33 @@ function numGen(){
     return num;
 }
 
-
+//CSS visual effects
 function button(btn){
     let myClick = btn.id;
     const myTime = setTimeout(responsive, 1000, btn);
     if( myClick == "box1"){
         btn.classList.add('lightUpYellow');
         myTime;
+        whatbox = "0";
     }
     else if( myClick == "box2"){
         btn.classList.add('lightUpBlue');
         myTime;
+        whatbox = "1";
     }
     else if( myClick == "box3"){
         btn.classList.add('lightUpRed');
         myTime;
+        whatbox = "2";
     }
     else if( myClick == "box4"){
         btn.classList.add('lightUpGreen');
         myTime;
+        whatbox ="3";
     }
-    whatbox = myClick;
     console.log(myClick);
     console.log(whatbox);
-   
+    
 }
 
 function responsive(btn){
@@ -93,18 +106,58 @@ function responsive2(place){
         
     }
 }
+function responsive3(place){
 
-function reset(){
-    location.reload();
+    if(place == "0"){
+        document.getElementById("box1").classList.remove('lightUpYellow');
+        
+    }
+    else if( place == "1"){
+        document.getElementById("box2").classList.remove('lightUpBlue');
+        
+    }
+    else if( place == "2"){
+        document.getElementById("box3").classList.remove('lightUpRed');
+        
+    }
+    else if( place == "3"){
+        document.getElementById("box4").classList.remove('lightUpGreen');
+        
+    }
 }
+function responsive4(place){
+
+    if(place == "0"){
+        document.getElementById("box1").classList.remove('lightUpYellow');
+        
+    }
+    else if( place == "1"){
+        document.getElementById("box2").classList.remove('lightUpBlue');
+        
+    }
+    else if( place == "2"){
+        document.getElementById("box3").classList.remove('lightUpRed');
+        
+    }
+    else if( place == "3"){
+        document.getElementById("box4").classList.remove('lightUpGreen');
+        
+    }
+}
+
+
+
+// runder
 
 function Start(){
     let place = numGen();
+    sequence1.push(place);
     const myTime2 = setTimeout(responsive2, 1000, place);
     console.log(place);
     if(place == "0"){
         document.getElementById("box1").classList.add('lightUpYellow');
         myTime2;
+
     }
     else if( place == "1"){
         document.getElementById("box2").classList.add('lightUpBlue');
@@ -118,28 +171,177 @@ function Start(){
         document.getElementById("box4").classList.add('lightUpGreen');
         myTime2;
     }
-let result = '';
-    if( result == "win"){
-       round2(); 
-    }
-    else if( result == "lose"){
-        Start();
-    }
-    
+    getInput();
 }
 
 function round2(){
-    let place1 = numGen();
-    let place2 = numGen();
-    let result2='';
-    if(result2 == "win"){
-        round3();
+    let place;
+    let timeFactor = 1;
+    for(let i = 0; i<5; i++){
+        place = numGen();
+        sequence2[i]=place;
     }
-    else if(result2 == "lose"){
-        Start();
+    let result2='';
+    for(let j=0; j <= sequence2.length; j++){   
+
+    if(sequence2[j] == "0"){
+        
+        if(sequence2[0] == sequence2[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+         document.getElementById("box1").classList.add('lightUpYellow');
+        timeDelay(j);   
+        }, 1000*j*timeFactor);
+    }
+    else if(sequence2[j] == "1"){
+        if(sequence2[0] == sequence2[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+          document.getElementById("box2").classList.add('lightUpBlue');
+        timeDelay(j);  
+        }, 1000*j*timeFactor);
+    }
+    else if(sequence2[j] == "2"){
+        if(sequence2[0] == sequence2[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+          document.getElementById("box3").classList.add('lightUpRed');
+        timeDelay(j);  
+        }, 1000*j*timeFactor);
+        
+    }
+    else if(sequence2[j] == "3"){
+        if(sequence2[0] == sequence2[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+          document.getElementById("box4").classList.add('lightUpGreen');
+        timeDelay(j);  
+        }, 1000*j*timeFactor);
+        
+    }
     }
 }
 function round3(){
+    let place;
+    let timeFactor = 1;
+    for(let i = 0; i<3; i++){
+        place = numGen();
+        sequence3[i]=place;
+    }
+    let result3='';
+    for(let j=0; j <= sequence3.length; j++){   
+
+    if(sequence3[j] == "0"){
+        
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+         document.getElementById("box1").classList.add('lightUpYellow');
+        timeDelay2(j);   
+        }, 1000*j*timeFactor);
+    }
+    else if(sequence3[j] == "1"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+          document.getElementById("box2").classList.add('lightUpBlue');
+        timeDelay2(j);  
+        }, 1000*j*timeFactor);
+    }
+    else if(sequence3[j] == "2"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+          document.getElementById("box3").classList.add('lightUpRed');
+        timeDelay2(j);  
+        }, 1000*j*timeFactor);
+        
+    }
+    else if(sequence3[j] == "3"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        setTimeout(function(){
+          document.getElementById("box4").classList.add('lightUpGreen');
+        timeDelay2(j);  
+        }, 1000*j*timeFactor);
+        
+    }
+    }
+}
+
+function round32(){
+    let place;
+    let timeFactor;
+    for(let i=0; i<3; i++){
+        place = numGen();
+        sequence3[i]=(place);
+    }
+    //let result3='';
+    for(let j=0; j <= sequence3.length; j++){   
+
+    if(sequence3[j] == "0"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        else if(sequence3[1] == sequence3[2]){
+            timeFactor = 3;
+        }
+        setTimeout(function(){
+         document.getElementById("box1").classList.add('lightUpYellow');
+        timeDelay2(j);   
+        }, 1000*j*timeFactor);
+        
+
+    }
+    else if(sequence3[j] == "1"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        else if(sequence3[1] == sequence3[2]){
+            timeFactor = 3;
+        }
+        setTimeout(function(){
+          document.getElementById("box2").classList.add('lightUpBlue');
+        timeDelay2(j);  
+        }, 1000*j*timeFactor);
+        
+    }
+    else if(sequence3[j] == "2"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        else if(sequence3[1] == sequence3[2]){
+            timeFactor = 3;
+        }
+        setTimeout(function(){
+          document.getElementById("box3").classList.add('lightUpRed');
+        timeDelay2(j);  
+        }, 1000*j*timeFactor);
+        
+    }
+    else if(sequence3[j] == "3"){
+        if(sequence3[0] == sequence3[1]){
+            timeFactor = 2;
+        }
+        else if(sequence3[1] == sequence3[2]){
+            timeFactor = 3;
+        }
+        setTimeout(function(){
+          document.getElementById("box4").classList.add('lightUpGreen');
+        timeDelay2(j);  
+        }, 1000*j*timeFactor);
+        
+    }
+    }
+
     let result3='';
     if(result3 == "win"){
         round4();
@@ -165,6 +367,54 @@ function round5(){
     else if(result5 == "lose"){
         Start();
     }
+}
+//user inputs
+function getInput(){
+    
+    document.getElementById("box1").addEventListener("click", function(){sequence1Player.push(0)})
+    document.getElementById("box2").addEventListener("click", function(){sequence1Player.push(1)})
+    document.getElementById("box3").addEventListener("click", function(){sequence1Player.push(2)})
+    document.getElementById("box4").addEventListener("click", function(){sequence1Player.push(3)})
+    
+    console.log(sequence1Player);
+}
+
+
+//tids delays
+function timeDelay(j){
+    setTimeout(responsive3, 1000+(100*j), sequence2[j]);
+}
+function timeDelay2(j){
+    setTimeout(responsive4, 1000+(100*j), sequence3[j]);
+}
+function timeDelay3(j){
+    setTimeout(responsive4, 1000+(100*j), sequence4[j]);
+}
+
+// evaluation
+
+function evaluate(pc, player){
+   let result = '';
+   for(let i=0; i<=player.length; i++){
+        if(pc[i] == player[i]){
+            console.log("yes");
+        }
+        else{ console.log("no");}
+        
+   }
+    if( result == "win"){
+       round2(); 
+    }
+    else if( result == "lose"){
+        Start();
+    } 
+}
+
+
+
+
+function reset(){
+    location.reload();
 }
 /*Oppgave - Simon Says[edit]
 Simon says!
